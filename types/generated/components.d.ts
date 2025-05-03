fb856c655e34c76cfc6c2c44f83351841e3f4f6d
kohze <robin@kohze.com>
2025-05-03 14:26:08 +0400

    updated strapi

---
+++ types/generated/components.d.ts
@@ -0,0 +1,43 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface ArticlesArticles extends Struct.ComponentSchema {
+  collectionName: 'components_articles_articles';
+  info: {
+    description: '';
+    displayName: 'articles';
+    icon: 'apps';
+  };
+  attributes: {
+    content: Schema.Attribute.RichText;
+    image: Schema.Attribute.JSON;
+    platform: Schema.Attribute.String;
+    seodescription: Schema.Attribute.String;
+    tag: Schema.Attribute.String;
+    timestamp: Schema.Attribute.Date & Schema.Attribute.DefaultTo<'2025-05-02'>;
+    title: Schema.Attribute.String & Schema.Attribute.Unique;
+  };
+}
+
+export interface ContractsContracts extends Struct.ComponentSchema {
+  collectionName: 'components_contracts_contracts';
+  info: {
+    displayName: 'contracts';
+    icon: 'archive';
+  };
+  attributes: {
+    content: Schema.Attribute.RichText;
+    image: Schema.Attribute.JSON;
+    seocontent: Schema.Attribute.String;
+    timestamp: Schema.Attribute.Date;
+    title: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'articles.articles': ArticlesArticles;
+      'contracts.contracts': ContractsContracts;
+    }
+  }
+}
